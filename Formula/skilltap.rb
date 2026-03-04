a516acd7c9e0f48b5aa18fadfc84712d3ea431d7
nklisch <47763054+nklisch@users.noreply.github.com>
2026-03-04 07:40:53 +0000

    Update skilltap to v0.5.3

--- Formula/skilltap.rb
+++ Formula/skilltap.rb
@@ -1,28 +1,28 @@
 class Skilltap < Formula
   desc "CLI for installing agent skills from any git host"
   homepage "https://github.com/nklisch/skilltap"
-  version "0.1.0"
+  version "0.5.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/nklisch/skilltap/releases/download/v#{version}/skilltap-darwin-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "14753d8164d3ffdbc112a62ba83be6d7a51747bf629a95fdd65258441db8fcb7"
     end
     on_intel do
       url "https://github.com/nklisch/skilltap/releases/download/v#{version}/skilltap-darwin-x64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "811a2824d4c77f5a69abc6895098bfa3390f0a5373a24684f588155870478264"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/nklisch/skilltap/releases/download/v#{version}/skilltap-linux-arm64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "108fd7622c13c9b15e6a0581166011f1e842ea468136dd56619e657e8d661e52"
     end
     on_intel do
       url "https://github.com/nklisch/skilltap/releases/download/v#{version}/skilltap-linux-x64"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "bfaa71c90664cf2b00623d048b5a97ec14258bee58d4fcbee076f45e9c3d5edb"
     end
   end
 
